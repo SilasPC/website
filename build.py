2913f8846c74feea18e307c43f741594d7263a6d
Silas <pockendahl@hotmail.com>
2024-06-18 18:51:58 +0200

    first commit

---
+++ build.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import re
+from os import listdir, read
+from os.path import isfile, join
+
+src_dir="src"
+out_dir="out"
+
+x = dict()
+for f in listdir(src_dir):
+	path = join(src_dir, f)
+	if not isfile(path): continue
+	with open(path) as fd:
+		x[f] = fd.read()
+
+cache = dict()
+def foo(p):
+	if cache.get(p) == 0:
+		raise "Cyclical template"
+	y = x[p]
+	cache[p] = 0
+	while True:
+		match = next(re.finditer(r"\{\{(.*)\}\}", y), None)
+		if match:
+			repl = foo(match.group(1).strip())
+			(s,e) = match.span()
+			y = y[:s] + repl + y[e:]
+		else:
+			break
+	cache[p] = y
+	with open(join(out_dir, p), mode="w") as fd:
+		fd.write(y)
+	return y
+
+for p in x.keys():
+	foo(p)
